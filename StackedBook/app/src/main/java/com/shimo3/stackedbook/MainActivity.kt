d2e37cf9ca6dbf1b247a93a3744a0a7e4c802fdd
shimo3 <shimo3@vega.ocn.ne.jp>
2020-12-23 21:48:13 +0900

    create new project
    Signed-off-by: shimo3 <shimo3@vega.ocn.ne.jp>

---
+++ StackedBook/app/src/main/java/com/shimo3/stackedbook/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.shimo3.stackedbook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
